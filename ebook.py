fef4e063f8755daad4257724087a8bdf8c75c21c
Skyshayde <Skyshayde@gmail.com>
2015-06-04 12:55:27 -0500

    Initial commit

---
+++ ebook.py
@@ -0,0 +1,97 @@
+# The MIT License (MIT)
+
+# Copyright (c) 2015 Teddy Heinen
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import zipfile
+import xml.etree.ElementTree as etree
+import os
+
+class Book:
+
+    """ Input should be {name}.{format} Currently, only epub is supported """
+    def __init__(self, name, format, ):
+        self.name = name
+        self.format = format
+        self.chapters = []
+        self.meta = {}
+
+        self.container_xml = ''
+        self.content_opf = ''
+        self.file = zipfile.ZipFile(self.name + "." + self.format, "w")
+
+    """ Build """
+    def build(self):
+        self.initEpub()
+        self.file.close()
+
+    def generate_container_XML(self):
+        container = etree.Element("container")
+
+        container.attrib['version'] = "1.0"
+        container.attrib['xmlns'] = "urn:oasis:names:tc:opendocument:xmlns:container"
+
+        rootfiles = etree.SubElement(container, "rootfiles")
+
+        rootfile = etree.SubElement(rootfiles, "rootfile")
+        rootfile.attrib['full-path'] = "assets/content.opf"
+        rootfile.attrib['media-type'] = "application/oebps-package+xml"
+
+        return etree.tostring(container, encoding="UTF-8")
+
+    def generate_content_opf(self):
+        pkg = etree.Element("package")
+        pkg.attrib['version'] = "2.0"
+        pkg.attrib['xmlns'] = "http://www.idpf.org/2007/opf"
+        pkg.attrib['unique-identifier'] = self.name
+
+        meta = etree.SubElement(pkg, "metadata")
+
+        for index in meta.items():
+            dc = etree.SubElement(pkg, "dc:" + index[0])
+            dc.text = index[1]
+
+        manifest = etree.SubElement(pkg, "manifest")
+        spine = etree.SubElement(pkg, "spine")
+        spine.attrib['toc'] = "ncx"
+        for index in self.chapters:
+            ch = etree.SubElement(manifest, "item")
+            ch.attrib['id'] = index.split('.')[0]
+            ch.attrib['href'] = "" + index
+            ch.attrib['media-type'] = "application/xhtml+xml"
+
+            ch_spine = etree.SubElement(spine, "itemref")
+            ch_spine.attrib['idref'] = ch.attrib['id']
+
+        return etree.tostring(pkg, encoding="UTF-8")
+
+    """ Initialize file for Epub """
+    def initEpub(self):
+        self.content_opf = self.generate_content_opf()
+        self.container_xml = self.generate_container_XML()
+        self.file.writestr("mimetype", "application/epub+zip")
+
+        self.file.writestr("META-INF/container.xml", self.container_xml)
+        self.file.writestr("assets/content.opf", self.content_opf)
+
+        for index in self.chapters:
+            f = open(index)
+            self.file.write(f.name, "assets/"+f.name)
+            print("writing " + f.name + " to zip")
