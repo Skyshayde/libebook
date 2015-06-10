08ec42aecff7faacffcf5701e5c68e8095177cb7
Skyshayde <Skyshayde@gmail.com>
2015-06-10 14:26:10 -0500

    Much work, primarily refactoring

--- ebook.py
+++ ebook.py
@@ -25,67 +25,84 @@ import xml.etree.ElementTree as etree
 
 class Book:
 
-    def __init__(self, name, fileformat, ):
-        self.name = name
-        self.format = fileformat
+    def __init__(self):
+        self.name = None
+        self.format = None
         self.chapters = []
         self.assets = []
         self.meta = {}
         self.file = None
 
+    def open(self):
+        return open("epubs/" + self.name + "." + self.format)
+        
     def build(self):
+        if self.name == None:
+            print("File name is not set")        
+        
+        if self.format == None:
+            print("Format is not set")
+        
+        if self.chapters == []:
+            print("No chapters have been added")
+
         if self.format == "epub":
             self.file = zipfile.ZipFile(self.name + "." + self.format, "w")
 
             self.file.writestr("mimetype", "application/epub+zip")
-
-            # Generate container.xml
-
-            container = etree.Element("container")
-
-            container.attrib['version'] = "1.0"
-            container.attrib['xmlns'] = "urn:oasis:names:tc:opendocument:xmlns:container"
-
-            rootfiles = etree.SubElement(container, "rootfiles")
-
-            rootfile = etree.SubElement(rootfiles, "rootfile")
-            rootfile.attrib['full-path'] = "assets/content.opf"
-            rootfile.attrib['media-type'] = "application/oebps-package+xml"
-
-            self.file.writestr("META-INF/container.xml", etree.tostring(container, encoding="UTF-8"))
-
-            # Generate content.opf
-
-            pkg = etree.Element("package")
-
-            pkg.attrib['version'] = "2.0"
-            pkg.attrib['xmlns'] = "http://www.idpf.org/2007/opf"
-            pkg.attrib['unique-identifier'] = self.name
-
-            metadata = etree.SubElement(pkg, "metadata")
-            for index in self.meta.items():
-                dc = etree.SubElement(metadata, "dc:" + index[0])
-                dc.text = index[1]
-
-            manifest = etree.SubElement(pkg, "manifest")
-
-            spine = etree.SubElement(pkg, "spine")
-            spine.attrib['toc'] = "ncx"
+            print("Writing mimetype")
             
-            for index in self.chapters:
-                item = etree.SubElement(manifest, "item")
-                item.attrib['id'] = index.name.split('.')[0]
-                item.attrib['href'] = index.name
-                item.attrib['media-type'] = "application/xhtml+xml"
+            self.file.writestr("META-INF/container.xml", generate_container_xml())
+            print("Writing container.xml")
 
-                spine_item = etree.SubElement(spine, "itemref")
-                spine_item.attrib['idref'] = item.attrib['id']
+            self.file.writestr("assets/content.opf", generate_content_opf(self.name, self.chapters, self.meta))
+            print("Writing content.opf")
 
-            self.file.writestr("assets/content.opf", etree.tostring(pkg, encoding="UTF-8"))
-
-            # Write chapters to epub
             for assets in self.chapters + self.assets:
-                self.file.write(assets.name, "assets/" + assets.name)
-                print("writing " + assets.name + " to zip")
+                self.file.writestr("assets/" + assets.name, assets.read())
+                print("Writing " + assets.name)
 
             self.file.close()
+
+
+def generate_container_xml():
+    container = etree.Element("container")
+
+    container.attrib['version'] = "1.0"
+    container.attrib['xmlns'] = "urn:oasis:names:tc:opendocument:xmlns:container"
+
+    rootfiles = etree.SubElement(container, "rootfiles")
+
+    rootfile = etree.SubElement(rootfiles, "rootfile")
+    rootfile.attrib['full-path'] = "assets/content.opf"
+    rootfile.attrib['media-type'] = "application/oebps-package+xml"
+    return etree.tostring(container, encoding="UTF-8")
+    self.file.writestr("META-INF/container.xml", etree.tostring(container, encoding="UTF-8"))
+
+def generate_content_opf(name, chapters, meta):
+    pkg = etree.Element("package")
+
+    pkg.attrib['version'] = "2.0"
+    pkg.attrib['xmlns'] = "http://www.idpf.org/2007/opf"
+    pkg.attrib['unique-identifier'] = name
+
+    metadata = etree.SubElement(pkg, "metadata")
+    for index in meta.items():
+        dc = etree.SubElement(metadata, "dc:" + index[0])
+        dc.text = index[1]
+
+    manifest = etree.SubElement(pkg, "manifest")
+
+    spine = etree.SubElement(pkg, "spine")
+    spine.attrib['toc'] = "ncx"
+    
+    for index in chapters:
+        item = etree.SubElement(manifest, "item")
+        item.attrib['id'] = index.name.split('.')[0]
+        item.attrib['href'] = index.name
+        item.attrib['media-type'] = "application/xhtml+xml"
+
+        spine_item = etree.SubElement(spine, "itemref")
+        spine_item.attrib['idref'] = item.attrib['id']
+
+    return etree.tostring(pkg, encoding="UTF-8")
